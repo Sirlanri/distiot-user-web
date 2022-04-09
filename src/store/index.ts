e93fd3557249bcf5b72f57c703c5f593ed97a028
深蓝 <mail@ri-co.cn>
2022-04-09 15:11:18 +0800

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,29 @@
+import { createStore } from 'vuex'
+
+const state = {
+ 
+}
+
+const mutations = {
+ 
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const modules = {
+
+}
+
+export default createStore({
+  state,
+  getters,
+  mutations,
+  actions,
+  modules
+})
