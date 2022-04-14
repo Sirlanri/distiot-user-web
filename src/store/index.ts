cc4c68597760633d9bbf5691cfd2cdc19387f514
深蓝 <mail@ri-co.cn>
2022-04-14 16:35:58 +0800

    新增 用户信息展示&绑定vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,27 @@
 import { createStore } from 'vuex'
 
 const state = {
- 
+ userInfo:{
+  mail:'',
+  level:1,
+  token:'',
+  dnum:0,
+ }
 }
 
 const mutations = {
- 
+  setUserInfo(state:any, userInfo: any) {
+    state.userInfo = userInfo
+  }
 }
 
 const getters = {
-
+  getUserToken:(state:any)=>{
+    return state.userInfo.token
+  },
+  isLogin:(state:any)=>{
+    return state.userInfo.token!=''
+  }
 }
 
 const actions = {
---
+++ src/views/user/userinfo.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+import {  onMounted } from 'vue';
+import http from '../../plugins/axios';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+
+onMounted(()=>{
+  if (!store.getters.isLogin) {
+    GetUserInfo()
+  }
+})
+
+//登录后 获取用户的信息
+function GetUserInfo() {
+  http.get('/getuserinfo')
+    .then(res=>{
+    if(res.status===200){
+      let data={
+        mail:'',
+        level:1,
+        token:'',
+        dnum:0,
+      }
+      data.dnum=res.data.dnum
+      data.level=res.data.level
+      data.mail=res.data.mail
+      data.token=res.data.token
+      store.commit('setUserInfo',data)
+    }
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+</script>
+<template>
+<div>
+  <v-card>
+    <v-card-title>
+      用户信息
+    </v-card-title>
+    <v-card-text>
+ 邮箱：{{store.state.userInfo.mail}}
+    </v-card-text>
+    <v-card-text>
+      用户Token：<br>
+      <span class="user-token">
+        {{store.state.userInfo.token}}
+      </span>
+    </v-card-text>
+  </v-card>
+</div>
+</template>
+<style>
+.user-token{
+  font-size: large;
+}
+</style>
