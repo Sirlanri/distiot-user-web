61bd2fe2bd802bf9a4a12ae0e95a08f9fc4f2118
深蓝 <mail@ri-co.cn>
2022-04-14 16:19:39 +0800

    将主页作为main

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'userinfo',
+      component: ()=>import('@/views/user/userinfo.vue')
     },
     {
       path: '/entry',
