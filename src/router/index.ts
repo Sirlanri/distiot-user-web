91204490d344378bacfbb663d8bbd140385cf101
ricoEcs1 <mail@ri-co.cn>
2022-05-01 20:48:03 +0800

    更改 路由使用hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import { useStore } from '../store/pinia';
 
 let store: any = null;
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
